01e34166936474ef865abdfbcbf7b9cfe9bb019f
Juan Bacardit Barrera <juanbacardit@hotmail.es>
2015-11-23 12:54:08 +0100

    Test corregido

--- .tests/meme.Spec.js
+++ .tests/meme.Spec.js
@@ -1,6 +1,6 @@
 
-///../src/meme.js
-describe("Modulo de generación de memes 'MemeGenerato' ",function(){
+//../src/meme.js
+describe("Modulo de generación de memes 'MemeGenerator' ",function(){
 	it("el creador sin parametros", function(){
 		var sut = new memeGenerator({model:"hello world", template:"template 1"});
 		expect(sut.text === "hello world").toBeTruthy();
